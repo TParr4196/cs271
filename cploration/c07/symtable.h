4b206fb155539db2f99d8b3c605faff1bfbbd2fc
Tyler Parr <parrri@oregonstate.edu>
2021-11-14 19:38:37 -0800

    cploration 07

---
+++ cploration/c07/symtable.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define SYMBOL_TABLE_SIZE 100
+
+typedef int16_t hack_addr;
+struct Symbol {
+  char* name;
+  hack_addr addr;
+} Symbol;
+struct Symbol* hashArray[SYMBOL_TABLE_SIZE];
+int hash(char *str);
+struct Symbol *find(char* key);
+void insert(char* key, hack_addr addr);
+void display_table();
