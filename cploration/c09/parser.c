d15e8d40b240a020a4b132193d361d4e621aff89
Tyler Parr <parrri@oregonstate.edu>
2021-11-28 22:16:48 -0800

    09

---
+++ cploration/c09/parser.c
@@ -0,0 +1,122 @@
+#include "parser.h"
+#include "symtable.h"
+
+void parse(FILE * file) {
+  char line[MAX_LINE_LENGTH] = {0};
+  unsigned int line_num=0;
+  unsigned int instr_num=0;
+  add_predefined_symbols();
+  while (fgets(line, sizeof(line), file)) {
+    line_num++;
+    if(instr_num>MAX_INSTRUCTIONS){
+      exit_program(EXIT_TOO_MANY_INSTRUCTIONS, MAX_INSTRUCTIONS + 1);
+    }
+    strip(line);
+    if (!*line) continue;
+    char inst_type = ' ';
+    if(is_Atype(line)){
+      inst_type='A';
+    }
+    if(is_label(line)){
+      inst_type='L';
+      char temp[strlen(line)-2];
+      extract_label(line, temp);
+      if(!isalpha(temp[0])){
+        exit_program(EXIT_INVALID_LABEL, line_num, line);
+      }
+      if(symtable_find(temp)!=NULL){
+        exit_program(EXIT_SYMBOL_ALREADY_EXISTS, line_num, line);
+      }
+      symtable_insert(temp, instr_num);
+      continue;
+    }
+    if(is_Ctype(line)){
+      inst_type='C';
+    }
+    if(inst_type != ' '){
+      printf("%c  %s \n",inst_type,line);
+    }
+    if(inst_type == ' ') {
+      printf("%s \n", line);
+    }
+    instr_num++;
+  }
+}
+
+char *strip(char *s){
+  char s_new[strlen(s)+1];
+  if (*s=='/' && *(s+1)=='/'){
+    s_new[0]='\0';
+    strcpy(s, s_new);
+    return s;
+  }
+  int i=0;
+  for (char *s2 = s; *s2; s2++) { 
+
+    if (!isspace(*s2)){
+      s_new[i++] = *s2;
+    }
+  s_new[i]='\0';
+  }
+  strcpy(s, s_new);
+  return s;
+}
+
+bool is_Atype(const char *line){
+  if(line[0]=='@'){
+    return true;
+  }
+  return false;
+}
+
+bool is_label(const char *line){
+  if(line[0] != '('){
+    return false;
+  }
+  if(line[strlen(line)-2] != ')'){
+    return false;
+  }
+  return true;
+}
+
+bool is_Ctype(const char *line){
+  return !(is_Atype(line) || is_label(line));
+}
+
+char *extract_label(const char *line, char* label){
+  int i;
+  for(i = 0; i<strlen(line)-3; i++){
+    label[i]=line[i+1];
+  }
+  label[i]='\0';
+  return label;
+}
+
+void add_predefined_symbols(){
+  int i;
+  predefined_symbol temp = {};
+  for(i = 0; i<NUM_PREDEFINED_SYMBOLS; i++){
+    temp=predefined_symbols[i];
+    symtable_insert(temp.name, temp.address);
+  }
+}
+
+bool parse_A_instruction(const char *line, a_instruction *instr){
+  char* s=(char*)malloc(strlen(line));
+  strcpy(s,line+1);
+  char s_end[MAX_LABEL_LENGTH]= NULL;
+  long result = strtol(s, &s_end, 10);
+  if(0==strcmp(s,s_end)){
+    instr->value.label=(char*)malloc(strlen(line));
+    strcpy(instr->value.label,s);
+    instr->is_addr=false;
+  }
+  else if(*s_end != 0){
+    return false;
+  }
+  else{
+    instr->value.address=result;
+    instr->is_addr=true;
+  }
+  return true;
+}
---
+++ cploration/c09/parser.h
@@ -0,0 +1,53 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <hack.h>
+
+#define MAX_LINE_LENGTH 200
+#define MAX_LABEL_LENGTH MAX_LINE_LENGTH-2
+#define MAX_HACK_ADDRESS INT16_MAX
+#define MAX_INSTRUCTIONS MAX_HACK_ADDRESS
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+char *strip(char *s);
+void parse(FILE * file);
+bool is_Atype(const char *);
+bool is_label(const char *);
+bool is_Ctype(const char *);
+void add_predefined_symbols();
+bool parse_A_instruction(const char *line, a_instruction *instr);
+char *extract_label(const char *line, char* label);
+typedef enum instr_type {
+  Invalid=-1,
+  Atype,
+  Ctype,
+} instr_type;
+typedef struct c_instruction {
+  opcode a:1;
+  opcode comp:6;
+  hack_addr dest:3;
+  opcode jump:3;
+} c_instruction;
+typedef struct a_instruction {
+  union value {
+    hack_addr address;
+    char* label;
+  } value;
+  bool is_addr:1;
+} a_instruction;
+typedef struct instruction{
+  union instr {
+    a_instruction a;
+    c_instruction c;
+  } instr;
+  instr_type type;
+} instruction;
+
+#endif
