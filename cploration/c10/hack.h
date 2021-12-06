f8e42a17bdb1ebcbbbcf5dfb7dfeb8db0029b56e
Tyler Parr <parrri@oregonstate.edu>
2021-12-05 20:43:51 -0800

    hack

--- cploration/c10/hack.h
+++ cploration/c10/hack.h
@@ -47,4 +47,193 @@ static const predefined_symbol predefined_symbols[NUM_PREDEFINED_SYMBOLS] = {{"R
 	{"THIS", SYM_THIS},{"THAT", SYM_THAT},
 };
 
+typedef enum jump_id {
+  JMP_INVALID = -1,
+  JMP_NULL,
+  JMP_JGT,
+  JMP_JEQ,
+  JMP_JGE,
+  JMP_JLT,
+  JMP_JNE,
+  JMP_JLE,
+  JMP_JMP,
+} jump_id;
+
+typedef enum dest_id {
+  DEST_INVALID = -1,
+  DEST_NULL,
+  DEST_M,
+  DEST_D,
+  DEST_DM,
+  DEST_A,
+  DEST_AM,
+  DEST_AD,
+  DEST_ADM,
+} dest_id;
+
+typedef enum comp_id {
+  COMP_INVALID = -1,
+  COMP_0 = 42,
+  COMP_1 = 63,
+  COMP_NEG_1 = 58,
+  COMP_D = 12,
+  COMP_A = 48,
+  COMP_M = 48,
+  COMP_NOT_D = 13,
+  COMP_NOT_A = 49,
+  COMP_NOT_M = 49,
+  COMP_NEG_D = 15,
+  COMP_NEG_A = 51,
+  COMP_NEG_M = 51,
+  COMP_D_PLUS_1 = 31,
+  COMP_A_PLUS_1 = 55,
+  COMP_M_PLUS_1 = 55,
+  COMP_D_MINUS_1 = 14,
+  COMP_A_MINUS_1 = 50,
+  COMP_M_MINUS_1 = 50,
+  COMP_D_PLUS_A = 2,
+  COMP_D_PLUS_M = 2,
+  COMP_D_MINUS_A = 19,
+  COMP_D_MINUS_M = 19,
+  COMP_A_MINUS_D = 7,
+  COMP_M_MINUS_D = 7,
+  COMP_D_AND_A = 0,
+  COMP_D_AND_M = 0,
+  COMP_D_OR_A = 21,
+  COMP_D_OR_M = 21,
+} comp_id;
+
+static inline jump_id str_to_jumpid(const char *s) {
+  jump_id id = JMP_INVALID;
+  if (s == NULL) {
+    id = JMP_NULL;
+  } else if (strcmp(s, "JGT") == 0) {
+    id = JMP_JGT;
+  } else if (strcmp(s, "JEQ") == 0) {
+    id = JMP_JEQ;
+  } else if (strcmp(s, "JGE") == 0) {
+    id = JMP_JGE;
+  } else if (strcmp(s, "JLT") == 0) {
+    id = JMP_JLT;
+  } else if (strcmp(s, "JNE") == 0) {
+    id = JMP_JNE;
+  } else if (strcmp(s, "JLE") == 0) {
+    id = JMP_JLE;
+  } else if (strcmp(s, "JMP") == 0) {
+    id = JMP_JMP;
+  }
+  return id;
+}
+
+static inline dest_id str_to_destid(const char *s) {
+  dest_id id = DEST_INVALID;
+  if (s == NULL) {
+    id = DEST_NULL;
+  } else if (strcmp(s, "M") == 0) {
+    id = DEST_M;
+  } else if (strcmp(s, "D") == 0) {
+    id = DEST_D;
+  } else if (strcmp(s, "DM") == 0) {
+    id = DEST_DM;
+  } else if (strcmp(s, "A") == 0) {
+    id = DEST_A;
+  } else if (strcmp(s, "AM") == 0) {
+    id = DEST_AM;
+  } else if (strcmp(s, "AD") == 0) {
+    id = DEST_AD;
+  } else if (strcmp(s, "M") == 0) {
+    id = DEST_ADM;
+  }
+  return id;
+}
+
+static inline comp_id str_to_compid(const char *s, int *a) {
+  comp_id id = COMP_INVALID;
+  if (strcmp(s, "0") == 0) {
+    id = COMP_0;
+    *a=0;
+  } else if (strcmp(s, "1") == 0) {
+    id = COMP_1;
+    *a=0;
+  } else if (strcmp(s, "-1") == 0) {
+    id = COMP_NEG_1;
+    *a=0;
+  } else if (strcmp(s, "D") == 0) {
+    id = COMP_D;
+    *a=0;
+  } else if (strcmp(s, "A") == 0) {
+    id = COMP_A;
+    *a=0;
+  } else if (strcmp(s, "M") == 0) {
+    id = COMP_M;
+    *a=1;
+  } else if (strcmp(s, "!D") == 0) {
+    id = COMP_NOT_D;
+    *a=0;
+  } else if (strcmp(s, "!A") == 0) {
+    id = COMP_NOT_A;
+    *a=0;
+  } else if (strcmp(s, "!M") == 0) {
+    id = COMP_NOT_M;
+    *a=1;
+  } else if (strcmp(s, "-D") == 0) {
+    id = COMP_NEG_D;
+    *a=0;
+  } else if (strcmp(s, "-A") == 0) {
+    id = COMP_NEG_A;
+    *a=0;
+  } else if (strcmp(s, "-M") == 0) {
+    id = COMP_NEG_M;
+    *a=1;
+  } else if (strcmp(s, "D+1") == 0) {
+    id = COMP_D_PLUS_1;
+    *a=0;
+  } else if (strcmp(s, "A+1") == 0) {
+    id = COMP_A_PLUS_1;
+    *a=0;
+  } else if (strcmp(s, "M+1") == 0) {
+    id = COMP_M_PLUS_1;
+    *a=0;
+  } else if (strcmp(s, "D-1") == 0) {
+    id = COMP_D_MINUS_1;
+    *a=0;
+  } else if (strcmp(s, "A-1") == 0) {
+    id = COMP_A_MINUS_1;
+    *a=0;
+  } else if (strcmp(s, "M-1") == 0) {
+    id = COMP_M_MINUS_1;
+    *a=1;
+  } else if (strcmp(s, "D+A") == 0) {
+    id = COMP_D_PLUS_A;
+    *a=0;
+  } else if (strcmp(s, "D+M") == 0) {
+    id = COMP_D_PLUS_M;
+    *a=1;
+  } else if (strcmp(s, "D-A") == 0) {
+    id = COMP_D_MINUS_A;
+    *a=0;
+  } else if (strcmp(s, "D-M") == 0) {
+    id = COMP_D_MINUS_M;
+    *a=1;
+  } else if (strcmp(s, "A-D") == 0) {
+    id = COMP_A_MINUS_D;
+    *a=0;
+  } else if (strcmp(s, "M-D") == 0) {
+    id = COMP_M_MINUS_D;
+    *a=1;
+  } else if (strcmp(s, "D&A") == 0) {
+    id = COMP_D_AND_A;
+    *a=0;
+  } else if (strcmp(s, "D&M") == 0) {
+    id = COMP_D_AND_M;
+    *a=1;
+  } else if (strcmp(s, "D|A") == 0) {
+    id = COMP_D_OR_A;
+    *a=0;
+  } else if (strcmp(s, "D|M") == 0) {
+    id = COMP_D_OR_M;
+    *a=1;
+  } else 
+}
+
 #endif
--- cploration/c10/parser.h
+++ cploration/c10/parser.h
@@ -31,9 +31,9 @@ typedef enum instr_type {
 } instr_type;
 typedef struct c_instruction {
   opcode a:1;
-  opcode comp:6;
-  hack_addr dest:3;
-  opcode jump:3;
+  opcode comp:7;
+  hack_addr dest:4;
+  opcode jump:4;
 } c_instruction;
 typedef struct a_instruction {
   union value {
@@ -50,5 +50,6 @@ typedef struct instruction{
   instr_type type;
 } instruction;
 bool parse_A_instruction(const char *line, a_instruction *instr);
+void parse_C_instruction(char *line, c_instruction *instr);
 
 #endif
